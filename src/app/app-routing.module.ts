0735fb9713ba854a837e749aecc4efb969cfccb4
Daniel Auberger <daniel.auberger@gmx.at>
2023-09-14 19:51:37 +0200

    change start screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  }/* ,
+  {
+    path: 'destinations',
+    loadChildren: () =>
+    import('./destinations/destination.module').then((m) => m.DestinationModule),
+  }, */];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
